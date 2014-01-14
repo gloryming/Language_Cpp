3bcac56db6e72dc9702d224d2ce8af9c25da00b6
gloryming <gloryming910222@gmail.com>
2014-01-14 13:58:00 +0800

    change files

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Users/YimingGuo/Desktop/PerformanceBenchmark/Language_Cpp/proj.android/../cocos2d/cocos/2d/ccFPSImages.c \
- /Users/YimingGuo/Desktop/PerformanceBenchmark/Language_Cpp/proj.android/../cocos2d/cocos/2d/ccFPSImages.h
-
-/Users/YimingGuo/Desktop/PerformanceBenchmark/Language_Cpp/proj.android/../cocos2d/cocos/2d/ccFPSImages.h:
